395ab68c7d241e558a36ba294bd91740d9e86a02
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-06-27 14:40:04 +0200

    version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'calculators/index'
+  # get 'calculator/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "calculators#index"
+  resources :calculators, only: [:index]
 end
